e95b2762ab21a5d3ed1cc87e3040d61df37f4388
Fedor Baart <fedor.baart@deltares.nl>
2013-09-08 12:24:17 +0200

    compiling version

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # Better install dflowfm, because otherwise it's hard to debug...
+#ESMF_DIR="/Users/fedorbaart/Documents/checkouts/unstruc/esmf"
 DFLOWFM_DIR="/Users/fedorbaart/Documents/checkouts/unstruc/build"
-SWAN_DIR="/Users/fedorbaart/Documents/checkouts/swandeltares"
+SWAN_DIR="/Users/fedorbaart/Documents/checkouts/chenopis/build"
 # find libswanesmf.so and libdflowfmesmf.so/dylib
-export LDFLAGS="-L${DFLOWFM_DIR}/lib -L${SWAN_DIR} "
+export LDFLAGS="-L${DFLOWFM_DIR}/lib -L${SWAN_DIR}/lib "
 # find unstruc_esmf.mod
-export FCFLAGS="-I${DFLOWFM_DIR}/include -I${SWAN_DIR} -g -O0"
+export FCFLAGS="-I${DFLOWFM_DIR}/include -I${SWAN_DIR}lib -g -O0"
 # this should not be necessary
 export FCFLAGS="$FCFLAGS  -ffree-line-length-none"
 
